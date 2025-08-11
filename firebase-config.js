b3110e6133dc5d1c583c12e6da048ea75ba604ea
rinkuchauhan9191-dot <rinkuchauhan9191@gmail.com>
2025-08-11 17:07:14 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDqDdvasu0UTUYl1ZZeLKFvCiJcmWk80-A",
   authDomain: "dispatch-f50ac.firebaseapp.com",
   projectId: "dispatch-f50ac",
-  storageBucket: "dispatch-f50ac.firebasestorage.app",
+  storageBucket: "dispatch-f50ac.appspot.com", // yaha fix kiya
   messagingSenderId: "898233336697",
   appId: "1:898233336697:web:8d96cb36c5766b519ea5a8",
   measurementId: "G-W5LMDX08P3"
