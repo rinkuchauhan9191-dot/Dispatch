492efe972ee616cb086b4983defa83755185447e
rinkuchauhan9191-dot <rinkuchauhan9191@gmail.com>
2025-08-11 16:52:58 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,14 @@
-<!-- Firebase SDKs -->
-<script src="https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js"></script>
+const firebaseConfig = {
+  apiKey: "AIzaSyDqDdvasu0UTUYl1ZZeLKFvCiJcmWk80-A",
+  authDomain: "dispatch-f50ac.firebaseapp.com",
+  projectId: "dispatch-f50ac",
+  storageBucket: "dispatch-f50ac.firebasestorage.app",
+  messagingSenderId: "898233336697",
+  appId: "1:898233336697:web:8d96cb36c5766b519ea5a8",
+  measurementId: "G-W5LMDX08P3"
+};
 
-<script>
-  // Tumhara Firebase config
-  const firebaseConfig = {
-    apiKey: "AIzaSyDqDdvasu0UTUYl1ZZeLKFvCiJcmWk80-A",
-    authDomain: "dispatch-f50ac.firebaseapp.com",
-    projectId: "dispatch-f50ac",
-    storageBucket: "dispatch-f50ac.firebasestorage.app",
-    messagingSenderId: "898233336697",
-    appId: "1:898233336697:web:8d96cb36c5766b519ea5a8",
-    measurementId: "G-W5LMDX08P3"
-  };
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const analytics = firebase.analytics();
-</script>
 
