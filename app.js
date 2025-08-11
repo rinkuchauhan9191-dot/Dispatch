6e3c0521872a5efe995d8a08cb955bdc919a8ddf
rinkuchauhan9191-dot <rinkuchauhan9191@gmail.com>
2025-08-11 17:05:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // app.js
-// Main application logic
+// Firebase initialization
+const firebaseConfig = {
+  apiKey: "AIzaSyDqDdvasu0UTUYl1ZZeLKFvCiJcmWk80-A",
+  authDomain: "dispatch-f50ac.firebaseapp.com",
+  projectId: "dispatch-f50ac",
+  storageBucket: "dispatch-f50ac.appspot.com",
+  messagingSenderId: "898233336697",
+  appId: "1:898233336697:web:8d96cb36c5766b519ea5a8",
+  measurementId: "G-W5LMDX08P3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get database & storage references
+const db = firebase.database();
+const storage = firebase.storage();
 
 // Elements
 const modal = document.getElementById('modal');
